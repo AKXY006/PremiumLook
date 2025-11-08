c1fe972095750e568ebab991098f0be74f3c7af9
Akshay <ak144336@gmail.com>
2025-11-08 14:52:34 +0530

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+// Reusable function to load external HTML (navbar & footer)
+function loadHTML(elementId, filePath) {
+  fetch(filePath)
+    .then(response => {
+      if (!response.ok) throw new Error("File not found: " + filePath);
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch(error => console.error(error));
+}
+
+
+
+loadHTML("footer", "../footer/footer.html");
